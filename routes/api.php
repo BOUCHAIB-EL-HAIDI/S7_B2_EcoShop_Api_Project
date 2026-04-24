d19b720ef2849990c4b3e7718e0615a28100af09
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2026-04-24 16:44:03 +0100

    refactor: align api routes with frontend requirements (/user and /orders)

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,14 @@ Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::get('/user', [AuthController::class, 'profile']);
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart', [CartController::class, 'store']);
     Route::put('/cart/{id}', [CartController::class, 'update']);
     Route::delete('/cart/{id}', [CartController::class, 'destroy']);
     Route::get('/orders', [OrderController::class, 'index']);
     Route::get('/orders/{id}', [OrderController::class, 'show']);
-    Route::post('/checkout', [OrderController::class, 'checkout']);
+    Route::post('/orders', [OrderController::class, 'checkout']);
 
 
 
